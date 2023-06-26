19c44090f065e9ac039589a0ccdfab5d605276c1
Lux <125393929+Lux-63@users.noreply.github.com>
2023-06-26 20:23:51 +0500

    Fix

--- script01.js
+++ script01.js
@@ -1,15 +1,14 @@
-﻿let showOnDisplay = document.querySelector('.display');
+﻿const showOnDisplay = document.querySelector('.display');
 
 function addValueAndCheckForDuplicateOperands(n) {
-    let specialChars = '/*-+%.';
-    let lastChar = showOnDisplay.innerHTML[showOnDisplay.innerHTML.length -1];
-    let firstOperatorCharacter = specialChars.includes(lastChar);
-    let secondOperatorCharacter = specialChars.includes(n);
-    let replacingOneCharacterWithAnother = showOnDisplay.innerHTML.slice(0, -1) + n;
+    const specialChars = '/*-+%.';
+    const lastChar = showOnDisplay.innerHTML[showOnDisplay.innerHTML.length -1];
+    const firstOperatorCharacter = specialChars.includes(lastChar);
+    const secondOperatorCharacter = specialChars.includes(n);
 
     //console.log(firstOperatorCharacter, secondOperatorCharacter, replacingOneCharacterWithAnother, n)
     if (firstOperatorCharacter === true && secondOperatorCharacter === true) {
-        showOnDisplay.innerHTML = replacingOneCharacterWithAnother;
+        showOnDisplay.innerHTML = showOnDisplay.innerHTML.slice(0, -1) + n;;
     } else {
         showOnDisplay.innerHTML += n;
     }
@@ -20,15 +19,17 @@ function clearInputField() {
     showOnDisplay.innerHTML = '';
 };
 
-function checkingThePercentageAndGettingTheResult() {
+function getResult() {
 
     //результат и вычисление процента
-    let checkForPercentage = showOnDisplay.innerHTML.includes('%')
-    let gettingTheValueOfTheAmount = showOnDisplay.innerHTML.substring(0, showOnDisplay.innerHTML.indexOf("%"));
-    let getPercentageValue = showOnDisplay.innerHTML.substring(showOnDisplay.innerHTML.indexOf("%") +1);
+    const checkForPercentage = showOnDisplay.innerHTML.includes('%')
 
     //console.log(checkForPercentage)
-    if (checkForPercentage == true) {     
+    if (checkForPercentage == true) {    
+        const numberAndPercentage = showOnDisplay.innerHTML.indexOf("%");
+        const gettingTheValueOfTheAmount = showOnDisplay.innerHTML.substring(0, numberAndPercentage);
+        const getPercentageValue = showOnDisplay.innerHTML.substring(numberAndPercentage +1);
+
         showOnDisplay.innerHTML = eval((gettingTheValueOfTheAmount /100) * getPercentageValue);
     } else {
         showOnDisplay.innerHTML = eval(showOnDisplay.innerHTML);
