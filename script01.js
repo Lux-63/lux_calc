77dcfbc855e218255fc38e0cb4c2385fce424e21
Lux <125393929+Lux-63@users.noreply.github.com>
2023-06-26 19:45:56 +0500

    indents

--- script01.js
+++ script01.js
@@ -2,17 +2,16 @@
 
 function addValueAndCheckForDuplicateOperands(n) {
     let specialChars = '/*-+%.';
-    let firstOperatorCharacter = specialChars.includes(showOnDisplay.innerHTML[showOnDisplay.innerHTML.length -1]);
+    let lastChar = showOnDisplay.innerHTML[showOnDisplay.innerHTML.length -1];
+    let firstOperatorCharacter = specialChars.includes(lastChar);
     let secondOperatorCharacter = specialChars.includes(n);
     let replacingOneCharacterWithAnother = showOnDisplay.innerHTML.slice(0, -1) + n;
 
     //console.log(firstOperatorCharacter, secondOperatorCharacter, replacingOneCharacterWithAnother, n)
-
-        if (firstOperatorCharacter == true && secondOperatorCharacter == true) {
-            showOnDisplay.innerHTML = replacingOneCharacterWithAnother;
-        }else {
-            showOnDisplay.innerHTML += n;
-        }
+    if (firstOperatorCharacter === true && secondOperatorCharacter === true) {
+        showOnDisplay.innerHTML = replacingOneCharacterWithAnother;
+    }else {showOnDisplay.innerHTML += n;
+    }
 };
 
 
@@ -21,17 +20,16 @@ function clearInputField() {
 };
 
 function checkingThePercentageAndGettingTheResult() {
-    //необходимо делать проверку символа % в случае соответствия переправлять на функцию процента
+
+    //результат и вычисление процента
     let checkForPercentage = showOnDisplay.innerHTML.includes('%')
     let gettingTheValueOfTheAmount = showOnDisplay.innerHTML.substring(0, showOnDisplay.innerHTML.indexOf("%"));
     let getPercentageValue = showOnDisplay.innerHTML.substring(showOnDisplay.innerHTML.indexOf("%") +1);
 
     //console.log(checkForPercentage)
-
-        if (checkForPercentage == true) {     
-            showOnDisplay.innerHTML = eval((gettingTheValueOfTheAmount /100) * getPercentageValue);
-    }else {
-            showOnDisplay.innerHTML = eval(showOnDisplay.innerHTML);
+    if (checkForPercentage == true) {     
+        showOnDisplay.innerHTML = eval((gettingTheValueOfTheAmount /100) * getPercentageValue);
+    }else {showOnDisplay.innerHTML = eval(showOnDisplay.innerHTML);
     }
 };
 
