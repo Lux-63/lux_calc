7844fb5a70c2f6cb26a56cc4a04afb83b32638f6
Lux <125393929+Lux-63@users.noreply.github.com>
2023-07-07 01:04:17 +0500

    fix %

--- script01.js
+++ script01.js
@@ -1,6 +1,7 @@
 ﻿const showOnDisplay = document.querySelector('.display');
 
 function addCharToDisplay(n) {
+    //замена оператора на новый нажатый оператор
     const specialChars = '/*-+%.';
     const lastChar = showOnDisplay.innerHTML[showOnDisplay.innerHTML.length -1];
     const firstOperatorCharacter = specialChars.includes(lastChar);
@@ -26,10 +27,6 @@ function getResult() {
 
     //console.log(checkForPercentage)
     if (checkForPercentage == true) {    
-        //const indexPercentage = showOnDisplay.innerHTML.indexOf("%");
-        //const baseValue = Number(showOnDisplay.innerHTML.substring(0, indexPercentage));
-        //const percentValue = Number(showOnDisplay.innerHTML.substring(indexPercentage +1));
-
         showOnDisplay.innerHTML = percentageOfAmount();
     } else {
         showOnDisplay.innerHTML = eval(showOnDisplay.innerHTML);
@@ -40,10 +37,38 @@ function removeLastCharacter() {
     showOnDisplay.innerHTML = showOnDisplay.innerHTML.slice(0, -1);
 };
 
-function percentageOfAmount(){
-    const indexPercentage = showOnDisplay.innerHTML.indexOf("%");
-    const baseValue = Number(showOnDisplay.innerHTML.substring(0, indexPercentage));
-    const percentValue = Number(showOnDisplay.innerHTML.substring(indexPercentage +1));
 
-    return eval((baseValue /100) * percentValue);
+//код с процентом, что бы можно было отнимать или складывать результаты (100-90%=10 или 100+90%=190)
+function percentageOfAmount(n){
+    if (showOnDisplay.innerHTML.includes('-') === true) {
+        return minusPercentageOfAmount(n);
+    } 
+    if (showOnDisplay.innerHTML.includes('+') === true) {
+        return plusPercentageOfAmount(n);
+    }
+    };
+function minusPercentageOfAmount(n){
+    const deleteChars = showOnDisplay.innerHTML.slice(0, -1);
+    const firstOperatorCharacter = deleteChars.includes('-');
+    const indexPercentage = deleteChars.indexOf('-');
+    const baseValue = Number(deleteChars.substring(0, indexPercentage));
+    const percentValue = Number(deleteChars.substring(indexPercentage +1));
+    //console.log(firstOperatorCharacter, indexPercentage, baseValue, percentValue)
+    
+    if (firstOperatorCharacter === true) {        
+    return eval(baseValue - (baseValue /100) * percentValue);
+    }
 };
+//надо сделать что бы работало в сложении
+function plusPercentageOfAmount(n){
+    const deleteChars = showOnDisplay.innerHTML.slice(0, -1);
+    const firstOperatorCharacter = showOnDisplay.innerHTML.includes('+');
+    const indexPercentage = showOnDisplay.innerHTML.indexOf('+');
+    const baseValue = Number(deleteChars.substring(0, indexPercentage));
+    const percentValue = Number(deleteChars.substring(indexPercentage +1));
+
+    //console.log(firstOperatorCharacter, indexPercentage, baseValue, percentValue)
+    if (firstOperatorCharacter === true) {            
+        return eval((baseValue - (baseValue /100) * percentValue)+ baseValue);
+    }
+    };
